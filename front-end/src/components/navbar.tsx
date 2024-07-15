89a623bd69cf0360e44c25d94d265bab306b4550
Pashypk23 <pashylabell22@gmail.com>
2024-07-16 00:59:34 +0100

    update5

--- front-end/src/components/navbar.tsx
+++ front-end/src/components/navbar.tsx
@@ -33,7 +33,7 @@ const BlueSkyNavbar: FC<Props> = ({ children }) => {
       setIsMiniPay(true);
       connect({ connector: injected({ target: "metaMask" }) });
     }
-  }, []);
+  }, [connect]);
 
   return (
     <>
