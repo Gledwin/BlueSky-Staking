ae13fb344a86ef23accada50e66ebe69760de539
Pashypk23 <pashylabell22@gmail.com>
2024-07-16 07:17:55 +0100

    update11

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -2,11 +2,15 @@
 
 import { useState, useEffect } from "react";
 import { useAccount, useConnect } from "wagmi";
+import BecomeAUser from "./become-a-user/page";
 import DisplayPanel from "@/components/DisplayPannel/DisplayPannel";
 import TokenApproval from "@/components/navigation/tokenApproval";
 import StakeAmount from "@/components/navigation/stakeAmount";
 import WithdrawAmount from "@/components/withdraw/withdraw";
 import Reward from "@/components/claimReward.tsx/claimReward";
+import { checkIfUserExists } from "@/services/checkIfUserExists";
+import { bookStoreUser } from "@/entities/bookStoreUser";
+import { getUserByWalletAddress } from "@/services/getUserByWalletAddress";
 import { injected } from "wagmi/connectors";
 
 export default function Home() {
@@ -14,6 +18,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const { connect } = useConnect();
   const { address, isConnected } = useAccount();
+  const [bookStoreUser, setBookStoreUser] = useState<bookStoreUser | null>(null);
   const [userAddress, setUserAddress] = useState("");
     const [isMounted, setIsMounted] = useState(false);
 
@@ -31,7 +36,28 @@ export default function Home() {
 
 
 
-  
+  useEffect(() => {
+    const checkIfUserExistsAndSet = async () => {
+      if (address) {
+        const doesUserExist = await checkIfUserExists(address);
+        setUserExists(doesUserExist);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
+    };
+
+    const fetchUserByWalletAddress = async () => {
+      const fetchedBookStoreUser = await getUserByWalletAddress(address, {
+        _walletAddress: address as `0x${string}`,
+      });
+
+      setBookStoreUser(fetchedBookStoreUser);
+    };
+
+    checkIfUserExistsAndSet();
+    fetchUserByWalletAddress();
+  }, [address, userExists, bookStoreUser]);
 
   const attemptConnection = async () => {
     if (window.ethereum && window.ethereum.isMiniPay) {
@@ -79,17 +105,12 @@ export default function Home() {
   return (
     <div className="bg-black text-white min-h-screen p-4">
       <div className="space-y-8">
-      <div className="flex flex-col items-center justify-center bg-gray-900 p-4 w-full rounded-md shadow-md">
-          <p>
-          Your address: {userAddress}
-
-          </p>
-        </div>
         <div className="flex flex-col items-center justify-center bg-gray-900 p-4 w-full rounded-md shadow-md">
           <p>
             Connected: <span className="badge">{isConnected.toString()}</span>
           </p>
         </div>
+        {userExists ? (
           <>
             <h1 className="text-4xl text-blue-500">BlueSky Staking-Rewards</h1>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full">
@@ -115,7 +136,9 @@ export default function Home() {
               </div>
             </div>
           </>
-       
+        ) : (
+          <BecomeAUser />
+        )}
       </div>
     </div>
   );
